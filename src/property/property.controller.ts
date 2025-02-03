2277d7f0800943648eab55e5920880a0e5c0e3ea
TeodorTod <teodor.at.tod@gmail.com>
2025-02-03 22:22:55 +0200

    property module

---
+++ src/property/property.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('property')
+export class PropertyController {
+
+    @Get()
+    findAll() {
+        return 'All properties'
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return id;
+    }
+
+
+    @Post()
+    create(@Body() body) {
+        return body;
+    }
+}
