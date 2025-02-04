05e7e79d972c6c228c574417bef7e1824868f8f3
TeodorTod <teodor.at.tod@gmail.com>
2025-02-04 21:57:22 +0200

    validators

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,21 +1,54 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreatePropertyDto } from './dto/createProperty.dto';
 
 @Controller('property')
 export class PropertyController {
+  @Get()
+  findAll() {
+    return 'All properties';
+  }
 
-    @Get()
-    findAll() {
-        return 'All properties'
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id, @Query('sort', ParseBoolPipe) sort) {
+    return id;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return id;
-    }
+  @Post()
+  create(
+    @Body(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        groups: ['create'],
+      }),
+    )
+    body: CreatePropertyDto,
+  ) {
+    return body;
+  }
 
-
-    @Post()
-    create(@Body() body) {
-        return body;
-    }
+  @Patch(':id')
+  update(
+    @Body(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        groups: ['update'],
+        always: true
+      }),
+    )
+    body: CreatePropertyDto,
+  ) {}
 }
