ba63f30c312e43856b616562abecfe68b07159b8
TeodorTod <teodor.at.tod@gmail.com>
2025-02-06 22:46:17 +0200

    property service

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,46 +1,57 @@
 import {
-    Body,
-    Controller,
-    Get,
-    Param,
-    ParseBoolPipe,
-    ParseIntPipe,
-    Patch,
-    Post,
-    Query,
-    UsePipes,
-    ValidationPipe,
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreatePropertyDto } from './dto/createProperty.dto';
 import { IdParamDto } from './dto/idParam.dto';
 import { ParseIdPipe } from './pipes/parseIdpipe';
+import { HeadersDto } from './dto/headers.dto';
+import { RequestHeader } from './pipes/request-header';
+import { PropertyService } from './property.service';
 
 @Controller('property')
 export class PropertyController {
-    @Get()
-    findAll() {
-        return 'All properties';
-    }
 
-    @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id, @Query('sort', ParseBoolPipe) sort) {
-        return id;
-    }
+    constructor(private propertyService: PropertyService) {}
 
-    @Post()
-    create(
-        @Body()
-        body: CreatePropertyDto,
-    ) {
-        return body;
-    }
+  @Get()
+  findAll() {
+    this.propertyService.findAll();
+  }
 
-    @Patch(':id')
-    update(
-        @Param('id', ParseIdPipe) id,
-        @Body()
-        body: CreatePropertyDto,
-    ) {
-        return body;
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id, @Query('sort', ParseBoolPipe) sort) {
+    this.propertyService.findOne();
+  }
+
+  @Post()
+  create(
+    @Body()
+    body: CreatePropertyDto,
+  ) {
+    this.propertyService.create();
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIdPipe) id,
+    @Body()
+    body: CreatePropertyDto,
+    @RequestHeader(
+      new ValidationPipe({ whitelist: true, validateCustomDecorators: true }),
+    )
+    header: HeadersDto,
+  ) {
+    this.propertyService.update();
+  }
 }
---
+++ src/property/property.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PropertyService {
+
+    findAll() {
+
+    }
+
+    findOne() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
